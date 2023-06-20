508591be1dae17fcb15f2e76d479343754ed6431
braaxL <135743603+braaxL@users.noreply.github.com>
2023-06-20 09:42:33 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atividade08
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            {
+                List<User> users = new List<User>();
+
+                // Adicionando usuários à lista
+                users.Add(new User("João", "joao@gmail.com"));
+                users.Add(new User("Maria", "maria@hotmail.com"));
+                users.Add(new User("Pedro", "pedro@yahoo.com"));
+
+                // Buscando usuário por email
+                string email = "maria@hotmail.com";
+                User user = users.Find(u => u.Email == email);
+
+                if (user != null)
+                {
+                    Console.WriteLine($"Usuário encontrado: {user.Name}");
+                }
+                else
+                {
+                    Console.WriteLine("Usuário não encontrado");
+                }
+            }
+        }
+
+        class User
+        {
+            public string Name { get; set; }
+            public string Email { get; set; }
+
+            public User(string name, string email)
+            {
+                Name = name;
+                Email = email;
+            }
+
+            
+        }
+    }
+}
